8fa5ed3671f8abe5f90257727c4e1dbd9592ba7b
mathieuhuet <mathieuhuet@live.ca>
2023-03-07 15:56:39 -0500

    Little adjustment

--- server/Controllers/user.js
+++ server/Controllers/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('./../models/user');
+const User = require('./../Models/user');
 const SECRET_KEY = process.env.SECRET_KEY || 'this isnt secure';
 
 
